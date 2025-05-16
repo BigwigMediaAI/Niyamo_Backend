8b0d0724029bfa943128f6706058cdd56079d24c
unknown <chandanpms@gmail.com>
2025-05-16 12:25:30 +0530

    database connected and server started

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 const express = require("express");
+const cors = require("cors");
+const { connect } = require("./config/db");
+
+require("dotenv").config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(process.env.PORT, async () => {
+  try {
+    await connect;
+    console.log("Connected to DB");
+  } catch (error) {}
+  console.log("Server is listening");
+});
